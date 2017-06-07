0d07f72db684653b08218fa3a1318bc9a92fd6ba
Smerilys <smerilys@gmail.com>
2017-06-07 11:40:08 +0200

    Update sensorsss_proc.ino

--- sensorsss_proc.ino
+++ sensorsss_proc.ino
@@ -24,31 +24,31 @@ HDC100X hdc(0x43);
 
 void setup() {
 
-/* Light Sensor */
+  /* Light Sensor */
 
-Serial.begin(9600);
-Wire.begin();
-Wire.beginTransmission(I2C_ADDR);
-Wire.write(0x80 | REG_CONTROL);
-Wire.write(0x03); // Power on
-Wire.endTransmission();
-Wire.beginTransmission(I2C_ADDR);
-Wire.write(0x80 | REG_CONFIG);
-Wire.write(0x00); // 400 ms
-Wire.endTransmission();
+  Serial.begin(9600);
+  Wire.begin();
+  Wire.beginTransmission(I2C_ADDR);
+  Wire.write(0x80 | REG_CONTROL);
+  Wire.write(0x03); // Power on
+  Wire.endTransmission();
+  Wire.beginTransmission(I2C_ADDR);
+  Wire.write(0x80 | REG_CONFIG);
+  Wire.write(0x00); // 400 ms
+  Wire.endTransmission();
 
-/* Air Pressure + temperature */
+  /* Air Pressure + temperature */
 
-if (!bmp.begin()) {
-  Serial.println("BMP init failed!");
-}
-bmp.setOversampling(4); // select resolution of the measurements 
+  if (!bmp.begin()) {
+    Serial.println("BMP init failed!");
+  }
+  bmp.setOversampling(4); // select resolution of the measurements 
 
-/* Humidity */
+  /* Humidity */
 
-hdc.begin(HDC100X_TEMP_HUMI, HDC100X_14BIT, DISABLE);
-  
-/* UV */
+  hdc.begin(HDC100X_TEMP_HUMI, HDC100X_14BIT, DISABLE);
+
+  /* UV */
   Wire.begin();
   Wire.beginTransmission(I2C_ADDR_UV);
   Wire.write((IT_1<<2) | 0x02);
@@ -60,7 +60,7 @@ hdc.begin(HDC100X_TEMP_HUMI, HDC100X_14BIT, DISABLE);
 
 void loop() {
 
-/* Light sensor */
+// Light sensor 
 
 LightSensor();
 
@@ -76,84 +76,4 @@ humidity();
 
 UVSensor();
 
-}
-
-void LightSensor()
-{  
-  Wire.beginTransmission(I2C_ADDR);
-  Wire.write(0x80 | REG_DATALOW);
-  Wire.endTransmission();
-  Wire.requestFrom(I2C_ADDR, 2); // Request 2 bytes
-  uint16_t low = Wire.read();
-  uint16_t high = Wire.read();
-  Serial.print("light : ");
-  Serial.println(low);
-
-  while (Wire.available()) {
-    Wire.read();
-  }
-  uint32_t lux;
-  lux = (high << 8) || (low << 0);
-  lux *= 1; // Multiplier for 400ms  
-}
-
-void PressureTemp()
-{
-  double temp, pressure;
-  char bmpStatus = bmp.startMeasurment();
-
-  // if an error occured on the sensor: stop
-  if (bmpStatus == 0) {
-  Serial.println(bmp.getError());
-  return;
-  }
-
-  delay(bmpStatus); // wait for duration of the measurement
-  bmpStatus = bmp.getTemperatureAndPressure(temp, pressure);
-
-  Serial.print("Temperature :");
-  Serial.print(temp);
-  Serial.println(" °C");
-  Serial.print("Pressure :");
-  Serial.print(pressure);
-  Serial.println(" hPa");
-}
-
-void humidity()
-{
-  Serial.print(hdc.getHumi());
-  Serial.println(" % of humidity");
-  Serial.println();
-}
-
-void UVSensor()
-{
-  byte msb=0, lsb=0; // first and second byte that will be read from the sensor
-  uint16_t uv;
-
-  Wire.requestFrom(I2C_ADDR_UV+1, 1); // MSB (read first byte from sensor)
-  delay(1);
-  if(Wire.available()) {
-    msb = Wire.read();
-  }
-
-  Wire.requestFrom(I2C_ADDR_UV+0, 1); // LSB (read second byte from sensor)
-  delay(1);
-  if(Wire.available()) {
-    lsb = Wire.read();
-  }
-
-  uv = (msb<<8) | lsb; // combine bytes to an integer through a bitshift op
-
-  Serial.print("μW per cm²: ");
-  Serial.println(uv, DEC);     // log value as 16bit integer
-  Serial.print("UV-Index: ");
-  Serial.println(getUVI(uv));
-
-  delay(1000);
-}
-
-float getUVI(int uv) {
-  float uvi = refVal * (uv * 5.625) / 1000;
-  return uvi;
 }
