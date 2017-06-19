161df4323c2515b020fdb266336f4101a773a4a3
Smerilys <smerilys@gmail.com>
2017-06-19 15:50:14 +0200

    Update SD_uS.ino

--- Complete_codes/SD_uS/SD_uS.ino
+++ Complete_codes/SD_uS/SD_uS.ino
@@ -2,9 +2,9 @@
 #include <SPI.h>
 #include <SD.h>
 
-// Pressure / temperature
+// humidity / temperature
 #include <Wire.h>
-#include <BMP280.h>
+#include <HDC100X.h>
 
 // Ultrasound sensor
 #include <NewPing.h>
@@ -16,8 +16,8 @@
 // Declare the Us sensor
 NewPing sensor(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
-// Declare the pressure / temp sensor
-BMP280 bmp;
+// Declare the humidity/temperature sensor
+HDC100X hdc(0x43);
 
 // Declare the file on the SD card
 File US_data;
@@ -29,31 +29,34 @@ File US_data;
   unsigned int echo_time; // Time it takes to the signal to come back to the sensor
   boolean state = 0; // What is happening in front of the sensor (no one / someone)
   
-  // Pressure / temperature
-  double temp, pressure;
-  char bmpStatus;
+  // Humidity/Temperature
+  float humidity, temperature;
+  
+  // Microphone
+  int mic = A0;
+  long micVal = 0;
+  long sound_lvl;
 
 void setup()
 {
   Serial.begin(115200);
   while(!Serial){;}
   
-  // Initialize the pressure/temperature sensor
-  if(!bmp.begin())
-  {
-    Serial.println("BMP init failed");
-  }
-  bmp.setOversampling(4); // resolution of measurements
+  // Initialize the humidity/temperature sensor
+  hdc.begin(HDC100X_TEMP_HUMI, HDC100X_14BIT, DISABLE);
   
   // Initialize the SD card
   if(!SD.begin(4))
   {
-    Serial.println("Failed to initialize. Stopping now");
-    // stop there
+    Serial.println("Failed to initialize. Stopping now...");
     for(;;);
   }
-  pinMode(10, OUTPUT);
-  // Create file on the SD card and close it instantly
+  
+  pinMode(10, OUTPUT); 
+  
+  pinMode(mic, INPUT);
+  
+  // Create files on the SD card and close it instantly
   US_data = SD.open("nb_ppl.txt", FILE_WRITE);
   US_data.close();
   if(!SD.exists("nb_ppl.txt"))
@@ -61,6 +64,7 @@ void setup()
     Serial.println("Failed to create 'nb_ppl.txt' file. Stopping now...");
     for(;;);
   }
+  // Start 
   Serial.println("starting");
 }
 
@@ -68,19 +72,33 @@ void loop()
 {
   delay(50); // Send ping about 20 times per second
   
-  bmpStatus = bmp.startMeasurment();
-  if (bmpStatus == 0) 
-  {
-    Serial.println(bmp.getError());
-    return;
-  }
-  
-  unsigned int echo_time = sensor.ping(); // Get time pings takes to come back in micro seconds
-  distance = sensor.convert_cm(echo_time); // Converts time to distance in cm
+  // Measure 
+    // Humidity/Temperature
+    humidity = hdc.getHumi();
+    temperature = hdc.getTemp();
+    
+    // Ultrasound
+    unsigned int echo_time = sensor.ping(); // Get time pings takes to come back in micro seconds
+    distance = sensor.convert_cm(echo_time); // Converts time to distance in cm
+    
+    // Microphone 
+    sound_lvl = getMicVal();
   
   // Printing part
   Serial.print("distance = ");
-  Serial.println(distance);
+  Serial.print(distance);
+  Serial.println(" cm");
+  
+  Serial.print("temperature = ");
+  Serial.print(temperature);
+  Serial.println(" Celsius");
+  
+  Serial.print("Humidity = ");
+  Serial.print(humidity);
+  Serial.println(" %");
+  
+  Serial.print("Sound level = ");
+  Serial.println(sound_lvl);
   
   // Update distance value and write on SD card if needed
   state = ppl_counter(state, distance, &nb_people);
@@ -119,11 +137,26 @@ int ppl_counter(int temp_state, int temp_dist, int *nb_people)
 void write_file(int *ppl)
 {
   (*ppl) += 1;
+  SD.remove("nb_ppl.txt");
   US_data = SD.open("nb_ppl.txt", FILE_WRITE);
   US_data.println(nb_people);
   US_data.close();
-  SD.remove("nb_ppl.txt");
   state == 0;
   Serial.print("SD card updated, one more person! Total of people :");
   Serial.println(*ppl);
+}
+
+// Get value of sound
+long getMicVal() 
+{
+  int period = 3; // Averaging three values ​​in order to catch any 'outliers'
+  int correction_value = 510;
+  for (int i = 0; i < period; i++) 
+  {
+    // Computes the absolute value of the value to intercept negative deflections
+    micVal = micVal + abs (analogRead(mic) - correction_value);
+    delay(5);
+  }
+  micVal = constrain(abs(micVal / period), 1, 500);
+  return (micVal);
 }
